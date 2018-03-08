1c93fa4d0c78d3ba398a29dad0af8ba4b492bb6a
fenglei3 <fenglei3@xin.com>
2018-03-08 19:14:16 +0800

    分支A 1

---
+++ gitDemo/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  gitDemo
+//
+//  Created by fenglei on 2018/3/8.
+//  Copyright © 2018年 fenglei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"git test");
+    NSLog(@"git second test");
+    NSLog(@"分支A 修改");
+    NSLog(@"master 修改");
+    NSLog(@"分支A second修改");
+    NSLog(@"分支A 1");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
